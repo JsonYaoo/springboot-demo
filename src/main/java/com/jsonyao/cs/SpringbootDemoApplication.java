80ab0fc9852a24b3936102e2676d5fc73a1a142f
yaocs2 <yaocs2@midea.com>
2020-11-26 10:10:40 +0800

    springboot-demo> demo参考链接更新

--- src/main/java/com/jsonyao/cs/SpringbootDemoApplication.java
+++ src/main/java/com/jsonyao/cs/SpringbootDemoApplication.java
@@ -3,6 +3,10 @@ package com.jsonyao.cs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Ralation:
+ *      a. https://www.jianshu.com/p/52bd327568b9
+ */
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
