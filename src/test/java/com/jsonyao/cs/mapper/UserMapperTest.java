8a4a9430acfdf6c44882f841e065e7d2f0bc6303
yaocs2 <yaocs2@midea.com>
2021-05-02 21:27:05 +0800

    springboot-demo> 添加redis

--- src/test/java/com/jsonyao/cs/mapper/UserMapperTest.java
+++ src/test/java/com/jsonyao/cs/mapper/UserMapperTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -18,10 +19,20 @@ public class UserMapperTest {
     @Resource
     private UserMapper userMapper;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Test
     public void findById(){
         Long id = 1000l;
         User user = userMapper.findById(id);
         System.out.println(user.toString());
+    }
+
+    @Test
+    public void testRedis(){
+        redisTemplate.opsForValue().set("name", "jsonyao");
+        String name = redisTemplate.opsForValue().get("name");
+        System.out.println(name);
     }
 }
